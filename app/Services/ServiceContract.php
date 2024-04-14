cc2ff2f0ceb943fcb1617875ae1c1e745c1b38d5
Arthur Weber <arthur2weber@gmail.com>
2024-04-14 09:04:06 -0300

    feat: configure redis connection

--- app/Services/ServiceContract.php
+++ app/Services/ServiceContract.php
@@ -5,12 +5,8 @@ namespace App\Services;
 interface ServiceContract
 {
     public function all(): ?array;
-
     public function find(int $id): ?array;
-
     public function create(array $data): ?array;
-
     public function update(int $id, array $data): ?array;
-
     public function delete(int $id): ?array;
 }
